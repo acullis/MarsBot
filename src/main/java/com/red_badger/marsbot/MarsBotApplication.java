19fa585b3dabaf2d51dddc89cdc12189e67a587a
Anarchy Games ZA <anarchygamesza@gmail.com>
2025-07-01 21:58:02 +0200

    Initial commit

---
+++ src/main/java/com/red_badger/marsbot/MarsBotApplication.java
@@ -0,0 +1,13 @@
+package com.red_badger.marsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/red_badger/marsbot/MarsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.red_badger.marsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
